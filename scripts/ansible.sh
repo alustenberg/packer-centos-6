9e0a7afedda63e09ff811f713a00829cd3139392
Alex <alustenberg@gmail.com>
2016-05-11 16:35:34 -0400

    use yum instead of pip to install
    - ran into issue with jinja2 not being installed properly when using pip
    - removed nfs as we do not need it

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -4,7 +4,5 @@
 rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 
 # Install Ansible.
-yum -y install git python-setuptools gcc sudo libffi-devel python-devel openssl-devel
 yum clean all
-easy_install pip
-pip install ansible
+yum -y install ansible python-setuptools
